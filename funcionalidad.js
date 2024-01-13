1179dcbf1229d22e15fd4016f2963eb37a2d719a
sergio1alanis <sergio1alanis@hotmail.com>
2024-01-13 11:25:54 -0600

    Final primera iteracion del codigo

--- funcionalidad.js
+++ funcionalidad.js
@@ -2,15 +2,16 @@ function comenzar() {
     const modal_btn1 = document.getElementById("modal-btn1");
     const modal_btn2 = document.getElementById("modal-btn2");
     const modal_container = document.getElementById("modal-container");
-    modal_btn1.addEventListener("click", () => createModal(modal_container, '<h3>Abrir modal</h3>'), false);
-    modal_btn2.addEventListener("click", () => createModal(modal_container, '<h3>Abrir modal 2</h3>'), false);
-
+    modal_btn1.addEventListener("click", () => createModal(modal_container, '<h3>Abrir modal</h3><img src="compuBrain.jpg" alt="">', "modal-desdeArriba"), false);
+    modal_btn2.addEventListener("click", () => createModal(modal_container, '<h3>Abrir modal 2</h3><a href="https://tianguis-de-plata-la-terminal.negocio.site/" target="_blank">Visita mi pagina</a>', "modal-desdeAbajo"), false);
 }
 
-function createModal(container, content = "contenido diverso") {
+
+
+function createModal(container, content = "contenido diverso", style = "") {
     container.innerHTML = `
         <div class="modal" id="modal">
-            <div class="modal-content">${content}
+            <div class="modal-content ${style}">${content}
                 <button class="modal-btn-closed" id="modal_btn_closed">X</button>
             </div>
         </div>
@@ -27,5 +28,10 @@ function createModal(container, content = "contenido diverso") {
         }
     });
 }
+
+window.addEventListener("load", () => {
+    const modalContainer = document.getElementById("modal-container");
+    createModal(modalContainer, '<div><h3 class="modal-title">Modal de ventana emergente</h3><img src="compuBrain.jpg" alt=""><p class="modal-descripcion">Esta es la descripcion del modal<p><button class="modal-btn">BOTON NO click</button><a href="https://tianguis-de-plata-la-terminal.negocio.site/" target="_blank">Visita mi pagina</a></div>', "modal-aparecer");
+});
 
 window.addEventListener("load", comenzar, false);
